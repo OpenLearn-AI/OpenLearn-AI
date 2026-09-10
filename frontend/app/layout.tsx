e10a71a462be4936809802384ef3adb57ddaedce
abdomoaz58-tech <abdomoaz58@gmail.com>
2026-09-10 17:04:59 +0300

    _

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-<<<<<<< Updated upstream
+
 
 import { ThemeProvider } from "@/components/theme-provider";
 
-=======
->>>>>>> Stashed changes
+
 //_____________________________________________________
 import { initSentry } from '../lib/sentry';
 initSentry();
